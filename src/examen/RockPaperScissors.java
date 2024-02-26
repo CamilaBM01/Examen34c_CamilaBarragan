ddd11f854ebba7339250ae0cedc42c3b75db73c2
Camila Barragan <damj01.2024@gmail.com>
2024-02-26 13:33:50 +0100

    Proyecto documentado

--- src/examen/RockPaperScissors.java
+++ src/examen/RockPaperScissors.java
@@ -1,15 +1,30 @@
 package examen;
 
 import java.util.*;
-
+/**
+ * La clase RockPaperScissors se trata de una clase que recibe dos strings mediante scanners y simula el juego de "piedra, papel o tijera".
+ * @author Camila
+ * @version 1.0
+ */
 public class RockPaperScissors {
+	//Keyboard es el primer atributo static inicializado a 0.
 	private static int keyboard = 0;
+	//scanner recibe nuestra respuesta
 	private static Scanner scanner = new Scanner(System.in);
+	/**
+	 * Se encarga de almacenar las 3 opciones en una lista de tipo array.list
+	 */
 	public static List<String> optionList;
-
+	/**
+	 * Es un constructor vacio.
+	 */
 	public RockPaperScissors() {
 	}
-
+	
+	/**
+	 * Al ejecutar la clase nos recibe con un print de bienvenida y la opcion de elegir que queremos tirar, con un numero del 1 al 3.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		optionList = Arrays.asList("Rock", "Paper", "Scissors");
 		do {
@@ -24,7 +39,14 @@ public class RockPaperScissors {
 		System.out.println(RockPaperScissors.checkGame(computerChoice, userChoice));
 		scanner.close();
 	}
-
+	
+	/**
+	 * Este método recibe la opcion que hemos elegido y la opcion que ha generado el anterior metodo de forma aleatoria.
+	 * Despues hace las comparaciones necesarias para determinar cual es el ganador, y manda el print que lo indica
+	 * @param userChoice
+	 * @param computerChoice
+	 * @return gameResult
+	 */
 	public static String checkGame(String userChoice, String computerChoice) {
 		String gameResult = "";
 		if (userChoice.equals(computerChoice)) {
